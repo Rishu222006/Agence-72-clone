0b1293c6b7edf230d9e51c45839f061a7f03fa08
Rishu222006 <vdkaushik33@gmail.com>
2025-11-29 00:43:49 +0530

    Finalised changes for the common link page.

--- src/components/common/com_link.jsx
+++ src/components/common/com_link.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Base from './footer'
+import { Link } from 'react-router-dom';
 
 const ComLink = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
     return (
         <div>
-            <div className='mt-[40vh] text-italics text-black text-[3vw] mb-[25vh] font-[font_rk1] text-center'>
-                <p>This site is not a
-                    <span className='font-bold transform transition-transform cursor-pointer hover:scale-110'> complete/exact </span>
-                    clone</p>
+            <div className='mt-[30vh] text-italics text-black text-[2vw] mb-[25vh] font-[font_rk1] text-center'>
+                <p>This site is not a complete/exact clone</p>
                 <p>of the original site hence not all</p>
                 <p>the pages and features have not been copied.</p>
+                <br />
+                <Link className='font-bold border-2 rounded-4xl p-4 transform inline-block hover:bg-black hover:text-white transition-transform hover:scale-125 border-black' to={'/'}>
+                    Back To Home Page
+                </Link>
             </div>
             <Base />
         </div>
